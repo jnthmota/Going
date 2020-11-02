a00b96c70eb7e6a08e004d50cd4c609f78dff31e
Jhow-Ms <jonathan.mota@outlook.com>
2020-11-02 15:32:36 -0300

    ArrayAppend

--- sectiontwo/Arrays/main2.go
+++ sectiontwo/Arrays/main2.go
@@ -7,7 +7,9 @@ import "fmt"
 func todo() {
 	//ar arr []int
 	arr := []int{1, 2, 3, 4}
-	arr2 := []string{"Hello", "Word", "I'm Jonathan"}
+	arr2 := []string{"Hello", "Word", "I'm"}
+
+	arr2 = append(arr2, "Jonathan")
 	fmt.Println(arr, arr2)
 }
 
