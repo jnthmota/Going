3204e6ceed7ed0a4d7cd3f0a169c348ffc23157c
Jhow-Ms <jonathan.mota@outlook.com>
2020-11-02 15:28:12 -0300

    packet

--- sectiontwo/Arrays/main.go
+++ sectiontwo/Arrays/main.go
@@ -1,17 +1,24 @@
 package main
 
-import "fmt"
+func swap(m1, m2 *int) {
+	var temp int
+	temp = *m2
+	*m2 = *m1
+	*m1 = temp
+}
+
+//import "fmt"
 
 //arrays
 
-func todo() {
-	//ar arr []int
-	arr := []int{1, 2, 3, 4}
-	fmt.Println(arr)
-}
+// func todo() {
+//ar arr []int
+//	arr := []int{1, 2, 3, 4}
+//	fmt.Println(arr)
+//}
 
-func main() {
-	todo()
+//func main() {
+//	todo()
 
-	//fmt.Println()
-}
+//fmt.Println()
+//}
---
+++ sectiontwo/Arrays/main2.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//arrays
+
+func todo() {
+	//ar arr []int
+	arr := []int{1, 2, 3, 4}
+	arr2 := []string{"Hello", "Word", "I'm Jonathan"}
+	fmt.Println(arr, arr2)
+}
+
+func main() {
+	todo()
+
+	//fmt.Println()
+}
