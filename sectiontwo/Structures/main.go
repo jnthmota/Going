dc7ee1356e1c4c92f7b7542503c30bf2cfac3c66
Jhow-Ms <jonathan.mota@outlook.com>
2020-11-04 23:32:40 -0300

    structure encapsulation func main another way

--- sectiontwo/Structures/main.go
+++ sectiontwo/Structures/main.go
@@ -12,7 +12,13 @@ type Car struct {
 }
 
 func main() {
-	c := Car{"Posche", 1, 2}
+	//c := Car{"Posche", 1, 2}
+	// another way
+	c := Car{
+		Name:    "Posche",
+		Age:     1,
+		ModelNo: 2,
+	}
 	//var c1 Car
 	fmt.Println(c)
 }
