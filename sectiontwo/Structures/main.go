a90b84fe0dcda0eb10a3c808a49403308dbc4e32
Jhow-Ms <jonathan.mota@outlook.com>
2020-11-04 23:29:18 -0300

    structure encapsulation func main

--- sectiontwo/Structures/main.go
+++ sectiontwo/Structures/main.go
@@ -5,11 +5,14 @@ import (
 )
 
 //structure encapsulation
-type Car struct{}
+type Car struct {
+	Name    string
+	Age     int
+	ModelNo int
+}
 
 func main() {
-	c := Car{}
-	var c1 Car
-
-	fmt.Println()
+	c := Car{"Posche", 1, 2}
+	//var c1 Car
+	fmt.Println(c)
 }
